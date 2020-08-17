e852d96c081d20c9cac6642b5c7dfa31bb497509
Steffen Burzlaff <burzlaffdesign@gmail.com>
2020-08-17 22:05:03 +0200

    Update videoDetails.py
    you can add thumbnail now as well by adding a .jpg file to the same folder.

--- videoDetails.py
+++ videoDetails.py
@@ -1,4 +1,5 @@
 import os
+from googleapiclient.http import MediaFileUpload
 
 class Video:
     description = "test description"
@@ -6,7 +7,21 @@ class Video:
     keywords = "test"
     privacyStatus = "private"
 
-    def getFileName():
+    def getFileName(self, type):
         for file in os.listdir("D:\\Videos\\Youtube\\Upload"):
-            return file
-            break
+            if type == "video" and file.split(".", 1)[1] != "jpg":
+                return file
+                break
+            elif type == "thumbnail" and file.split(".", 1)[1] != "mp4":
+                return file
+                break
+
+    def insertThumbnail(self, youtube, videoId):
+        thumnailPath = "D:\\Videos\\Youtube\\Upload\%s" % (self.getFileName("thumbnail"))
+
+        request = youtube.thumbnails().set(
+            videoId=videoId,
+            media_body=MediaFileUpload(thumnailPath)
+        )
+        response = request.execute()
+        print(response)
